576b6051b7285275d50c912b35d8481a253aea32
"lazycoder1" <"gautamgsabhahit@gmail.com">
2021-11-17 01:48:23 +0530

    Adding fund management contract and fixing import errors

--- Contracts/erc721.sol
+++ Contracts/erc721.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC721.sol";
+import "./Interfaces/IERC721.sol";
 import "./IERC721Receiver.sol";
 import "./Interfaces/IERC721Metadata.sol";
 import "./utils/Address.sol";
--- Contracts/utils/Address.sol
+++ Contracts/utils/Address.sol
@@ -1,4 +1,4 @@
-/ SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.3.2 (utils/Address.sol)
 
 pragma solidity ^0.8.0;
